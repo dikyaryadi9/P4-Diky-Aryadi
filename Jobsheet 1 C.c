c3da4b3549844ae08abf2843b71db91606c113db
dikyaryadi9 <65202718+dikyaryadi9@users.noreply.github.com>
2020-05-12 11:52:13 +0700

    Create Jobsheet 1 C.c
    Membuat program array satu dimensi yang menampilkan nim masing masing mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+main()
+{
+	int my_array[11] = { 1,23,17,4,-5,100,8,7,6,10,21};
+	printf("Data ke-1 = %d",my_array[0]);
+	printf("\nData ke-2 = %d",my_array[1]);
+	printf("\nData ke-3 = %d",my_array[2]);
+	printf("\nData ke-4 = %d",my_array[3]);
+	printf("\nData ke-5 = %d",my_array[4]);
+	printf("\nData ke-6 = %d",my_array[5]);
+	printf("\nData ke-7 = %d",my_array[6]);
+	printf("\nData ke-8 = %d",my_array[7]);
+	printf("\nData ke-9 = %d",my_array[8]);
+	printf("\nData ke-10 = %d",my_array[9]);
+	printf("\nData ke-11 = %d",my_array[10]);
+}
